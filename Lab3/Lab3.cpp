747115f9323f1f2791c6152fff27a8626f83a540
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-05-22 12:39:53 -0400

    Replaced ttf files, got text to print with mouse location

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
@@ -29,11 +30,12 @@ int main()
 	al_init_font_addon();
 	al_init_ttf_addon();
 
-	ALLEGRO_FONT* font24 = al_load_font("Resident Evil Large.ttf", 24, 0);
+	ALLEGRO_FONT* font24 = al_load_font("Movistar Text Regular.ttf", 24, 0);
 	eventQueue = al_create_event_queue();
 
 	al_register_event_source(eventQueue, al_get_display_event_source(display));
 	al_clear_to_color(al_map_rgb(255, 255, 255));
+	al_flip_display();
 
 	if (!al_install_mouse()) {
 		al_show_native_message_box(NULL, "Error", "Mouse was not initialized", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
@@ -43,8 +45,7 @@ int main()
 	al_register_event_source(eventQueue, al_get_mouse_event_source());
 
 	while (!done) {
-		al_flip_display();
-
+	
 		ALLEGRO_EVENT Event;
 		al_wait_for_event(eventQueue, &Event);
 		if (Event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
@@ -60,9 +61,13 @@ int main()
 		if (color) {
 			if (xpos >= 0 && xpos <= (width/2) && ypos >= 0 && ypos <= height) {
 				al_clear_to_color(al_map_rgb(255, 0, 0));
+				al_draw_textf(font24, al_map_rgb(0, 0, 255), xpos, ypos, ALLEGRO_ALIGN_LEFT, "The mouse is located at = %i / %i", xpos, ypos);
+				al_flip_display();
 			}
 			else if (xpos >= (width / 2) && xpos <= width && ypos >= 0 && ypos <= height) {
 				al_clear_to_color(al_map_rgb(0, 0, 255));
+				al_draw_textf(font24, al_map_rgb(255, 0, 0), xpos, ypos, ALLEGRO_ALIGN_LEFT, "The mouse is located at = %i / %i", xpos, ypos);
+				al_flip_display();
 			}
 		}
 	}
